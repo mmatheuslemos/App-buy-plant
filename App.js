da93c18a2b4f908931c9799f9eb864275f1bd608
mmatheulemos <matheusfelipelemos@hotmail.com>
2022-08-14 19:25:33 -0300

    Atual

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './components/HomeScreen';
+import DetailsScreen from './components/DetailsScreen';
+import COLORS from './const/colors';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <StatusBar barStyle='dark-content' backgroundColor={Colors.white}/>
+        <Stack.Navigator screenOptions={{header: ()=>null}}>
+            <Stack.Screen name='Home' component={HomeScreen}/>
+            <Stack.Screen name='Details' component={DetailsScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
